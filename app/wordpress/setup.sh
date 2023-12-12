cfd768aac46b630a41573d308826936a4f6715c0
jperez <03.jon.perez@gmail.com>
2023-12-12 17:06:18 +0100

    me escarga wordpress, pero no en el volumen

--- app/wordpress/setup.sh
+++ app/wordpress/setup.sh
@@ -6,5 +6,5 @@ tar -xvzf /var/www/html/example.com/latest.tar.gz -C /var/www/html/example.com/
 rm /var/www/html/example.com/latest.tar.gz
 
 # Iniciar el servicio necesario o cualquier otro comando que mantenga el contenedor en ejecución
-# exec tail -f /dev/null
+tail -f
 
