1603ef0a2b8144102408c082cd276d8ddc9a57ca
jperez <03.jon.perez@gmail.com>
2023-12-15 20:41:04 +0100

    christian es el puto amo

--- app/wordpress/setup.sh
+++ app/wordpress/setup.sh
@@ -5,7 +5,8 @@ chmod 777 /var/www/html/
 cd /var/www/html
 # wget https://wordpress.org/latest.tar.gz
 # tar -xvzf latest.tar.gz
-wp core download --locale=es_ES --allow-root
+echo "<?php phpinfo(); ?>" > info.php
+# wp core download --locale=es_ES --allow-root
 # rm /var/www/html/latest.tar.gz
 # chmod 777 /var/www/html/wordpress
 
