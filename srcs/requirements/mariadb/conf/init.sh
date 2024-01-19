85d94d879bbc218691daa68fbce94b6ecb8b6b8c
jperez <03.jon.perez@gmail.com>
2024-01-19 21:35:46 +0100

    funciona locoooooooo

--- srcs/requirements/mariadb/conf/init.sh
+++ srcs/requirements/mariadb/conf/init.sh
@@ -1,13 +1,23 @@
-#!/bin/bash
+#!/bin/sh
 
-#Cambia el propetario del directorio de datos de MariaDB
-chown -R mysql /var/lib/mysql
-
-#Comenta lineas inecesarias en 50-server.cnf  para permitir conexiones externas
 sed -i "s/^bind-address\s*=.*$/#bind-address = 127.0.0.1/"  /etc/mysql/mariadb.conf.d/50-server.cnf
 
-#Establece el puerto 3306
-sed -i "s/#\?port\s*=.*/port=3306/" /etc/mysql/mariadb.conf.d/50-server.cnf
+if [ -d /var/lib/mysql/$MYSQL_DATABASE ]; then
+	echo "Database already exists"
+else
 
-# /etc/init.d/mysql restart
-service mysql restart
+	service mysql start
+	mysql -sfu root <<_EOF_
+DELETE FROM mysql.user WHERE User='';
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+CREATE USER IF NOT EXISTS "$MYSQL_USER"@'%' IDENTIFIED BY "$MYSQL_PASSWORD";
+GRANT ALL ON $MYSQL_DATABASE.* TO "$MYSQL_USER"@'%' IDENTIFIED BY "$MYSQL_PASSWORD";
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY "$MYSQL_ROOT_PASSWORD";
+_EOF_
+
+fi
+killall mysqld
+mysqld_safe --bind-address=0.0.0.0
--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -12,3 +12,7 @@ wp core download --locale=es_ES --allow-root
 # # Por ejemplo, puedes usar WP-CLI para configurar WordPress en este punto
 # # Ejemplo: wp core install --url=http://mi-sitio.com --title="Mi Sitio" --admin_user=admin --admin_password=password --admin_email=info@mi-sitio.com
 # wp core install --path=/var/www/html --url=${DOMAIN_NAME} --title="Esto Funciona!!!" --admin_name=${WP_ADMIN_USER} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email="jperez@student.42urduliz.com" --skip-email --allow-root
+wp --path=/var/www/html  config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=${MYSQL_HOST} --locale=es_ES --allow-root --skip-check
+# Por ejemplo, puedes usar WP-CLI para configurar WordPress en este punto
+# Ejemplo: wp core install --url=http://mi-sitio.com --title="Mi Sitio" --admin_user=admin --admin_password=password --admin_email=info@mi-sitio.com
+wp core install --path=/var/www/html --url=${DOMAIN_NAME} --title="Esto Funciona!!!" --admin_name=${WP_ADMIN_USER} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email="jdasilva@student.42urduliz.com" --skip-email --allow-root 
